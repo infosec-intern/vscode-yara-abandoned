703c8cb1af9f0fcf02ea0220599ebbd1dd77dcca
Infosec-Intern <thomas@infosec-intern.com>
2018-12-07 22:30:18 -0700

    switched transport to socket-based

--- client/extension.ts
+++ client/extension.ts
@@ -1,5 +1,7 @@
 "use strict";
 
+import {execSync} from "child_process";
+import {Socket} from "net";
 import * as path from "path";
 import {ExtensionContext} from "vscode";
 import * as lcp from "vscode-languageclient";
@@ -8,11 +10,20 @@ import * as lcp from "vscode-languageclient";
 export function activate(context: ExtensionContext) {
     let serverPath: string = context.asAbsolutePath(path.join("server", "languageServer.py"));
     let pythonPath: string = context.asAbsolutePath(path.join("server", "env", "Scripts", "python"));
-    const serverOptions: lcp.ServerOptions = {
-        command: pythonPath,
-        args: [serverPath],
-        transport: lcp.TransportKind.stdio
-    };
+    const serverOptions: lcp.ServerOptions = function() {
+        // execSync(`${pythonPath} ${serverPath}`);
+		return new Promise((resolve, reject) => {
+            var client = new Socket();
+            console.log("created socket");
+			client.connect(8471, "127.0.0.1", function() {
+                console.log(`client connected to ${client.localAddress}:${client.localPort}`);
+				resolve({
+					reader: client,
+					writer: client
+				});
+			});
+		});
+	}
     const clientOptions: lcp.LanguageClientOptions = {
         documentSelector: [{ scheme: "file", language: "yara" }],
         diagnosticCollectionName: "yara",
--- server/languageServer.py
+++ server/languageServer.py
@@ -1,9 +1,13 @@
 ''' Implements a VSCode language server for YARA '''
 import asyncio
 import http
+from io import BufferedReader, TextIOWrapper
 import json
 import logging
+import logging.handlers
+import signal
 import sys
+from typing import Tuple
 
 
 try:
@@ -14,51 +18,42 @@ except ModuleNotFoundError:
     logging.warning("yara-python not installed. Diagnostics will not be available")
     HAS_YARA = False
 
+LOGGER = logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-def _binary_stdio():
-    '''
-    Construct binary stdio streams (not text mode).
-    This seems to be different for Window/Unix Python2/3, so going by:
-        https://stackoverflow.com/questions/2850893/reading-binary-data-from-stdin
 
-    Thanks Palantir!
-    https://github.com/palantir/python-language-server/blob/ab3e5eaef848a0cc752110f85ed95187f5cffcc4/pyls/__main__.py
-    '''
-    if sys.version_info >= (3, 0):
-        # pylint: disable=no-member
-        stdin, stdout = sys.stdin.buffer, sys.stdout.buffer
-    else:
-        # Python 2 on Windows opens sys.stdin in text mode, and
-        # binary data that read from it becomes corrupted on \r\n
-        if sys.platform == "win32":
-            # set sys.stdin to binary mode
-            # pylint: disable=no-member,import-error
-            import os
-            import msvcrt
-            msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
-            msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
-        stdin, stdout = sys.stdin, sys.stdout
-    return stdin, stdout
+def _build_logger() -> logging.Logger:
+    ''' Build the loggers '''
+    global LOGGER
+    LOGGER = logging.getLogger(__name__)
+    screen_hdlr = logging.StreamHandler()
+    screen_hdlr.setFormatter(logging.Formatter("%(message)s"))
+    screen_hdlr.setLevel(logging.INFO)
+    file_hdlr = logging.handlers.RotatingFileHandler(filename=".yara.log", backupCount=1, maxBytes=100000)
+    file_hdlr.setFormatter(logging.Formatter("%(levelname)s [%(module)s:%(lineno)d] %(message)s"))
+    file_hdlr.setLevel(logging.DEBUG)
+    LOGGER.addHandler(screen_hdlr)
+    LOGGER.addHandler(file_hdlr)
+    LOGGER.setLevel(logging.DEBUG)
 
-def code_completion_provider(request: dict):
+def code_completion_provider(request: dict) -> str:
     ''' Respond to the completionItem/resolve request '''
-    logging.warning("code_completion_provider() is not yet implemented")
+    LOGGER.warning("code_completion_provider() is not yet implemented")
 
-def definition_provider(request: dict):
+def definition_provider(request: dict) -> str:
     ''' Respond to the textDocument/definition request '''
-    logging.warning("definition_provider() is not yet implemented")
+    LOGGER.warning("definition_provider() is not yet implemented")
     return {
         "jsonrpc": "2.0",
         "id": request["id"],
         "result": {}
     }
 
-def diagnostic_provider(request: dict):
+def diagnostic_provider(request: dict) -> str:
     ''' Respond to the textDocument/publishDiagnostics request
     The message carries an array of diagnostic items for a resource URI.
     '''
     if HAS_YARA:
-        logging.warning("diagnostic_provider() is not yet implemented")
+        LOGGER.warning("diagnostic_provider() is not yet implemented")
         logging.debug("yara-python is installed")
         response = {
             "jsonrpc": "2.0",
@@ -66,41 +61,43 @@ def diagnostic_provider(request: dict):
             "result": {}
         }
     elif not HAS_YARA:
-        logging.error("yara-python is not installed. Diagnostics are disabled")
+        LOGGER.error("yara-python is not installed. Diagnostics are disabled")
         response = {}
     return response
 
-def highlight_provider(request: dict):
+def highlight_provider(request: dict) -> str:
     ''' Respond to the textDocument/documentHighlight request '''
-    logging.warning("highlight_provider() is not implemented")
+    LOGGER.warning("highlight_provider() is not implemented")
     return {
         "jsonrpc": "2.0",
         "id": request["id"],
         "result": {}
     }
 
-def reference_provider(request: dict):
+def reference_provider(request: dict) -> str:
     ''' Respond to the textDocument/references request '''
-    logging.warning("reference_provider() is not yet implemented")
+    LOGGER.warning("reference_provider() is not yet implemented")
     return {
         "jsonrpc": "2.0",
         "id": request["id"],
         "result": {}
     }
 
-def rename_provider(request: dict):
+def rename_provider(request: dict) -> str:
     ''' Respond to the textDocument/rename request '''
-    logging.warning("rename_provider() is not yet implemented")
+    LOGGER.warning("rename_provider() is not yet implemented")
     return {
         "jsonrpc": "2.0",
         "id": request["id"],
         "result": {}
     }
 
-def initialize():
+@asyncio.coroutine
+async def initialize(reader:asyncio.StreamReader, writer: asyncio.StreamWriter) -> str:
     ''' Announce language support methods '''
     # document_selector = { "language": "yara", "scheme": "file" }
-    announcement = {
+    LOGGER.debug("inside initialize()")
+    announcement = json.dumps({
         "capabilities": {
             # "codeActionProvider": False,
             # "codeLensProvider": False,
@@ -120,16 +117,24 @@ def initialize():
             # "signatureHelpProvider": False,
             # "workspaceSymbolProvider": False,
         }
-    }
-    return json.dumps(announcement)
+    }).encode()
+    writer.write(announcement)
+    await writer.drain()
 
-def main():
-    logging.info("Starting YARA IO language server")
-    stdin, stdout = _binary_stdio()
-    logging.debug("rfile: %s", stdin)
-    logging.debug("wfile: %s", stdout)
+async def main():
+    ''' Program entrypoint '''
+    LOGGER.info("Starting YARA IO language server")
+    server = await asyncio.start_server(initialize, "127.0.0.1", 8471)
+    LOGGER.info("Serving on {}".format(server.sockets[0].getsockname()))
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format="%(levelname)s [%(module)s:%(lineno)d] %(message)s")
-    main()
+    try:
+        _build_logger()
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        LOGGER.critical("Stopping at user's request")
+    except Exception as err:
+        LOGGER.exception(err)
