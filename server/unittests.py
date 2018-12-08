685f3aeb70e6aab3fc3fbaefcbe99cbd8911487a
Infosec-Intern <thomas@infosec-intern.com>
2018-12-08 10:06:51 -0700

    added more imports

--- server/unittests.py
+++ server/unittests.py
@@ -1,37 +1,56 @@
+import asyncio
+import logging
+import socket
 import unittest
+
+import languageServer as server
 
 
 class TestLanguageServer(unittest.TestCase):
+    @classmethod
     def setUpClass(self):
         ''' Initialize tests '''
-        pass
+        server._build_logger()
+        # asyncio.run(server.main())
+
+    @classmethod
     def tearDownClass(self):
         ''' Clean things up '''
         pass
+
     def test_code_completion_provider(self):
         ''' Test code completion provider '''
         pass
+
     def test_definition_provider(self):
         ''' Test defintion provider '''
         pass
+
     def test_diagnostic_provider(self):
         ''' Test diganostic provider '''
         pass
+
     def test_exceptions_handled(self):
         ''' Test the server handles exceptions properly '''
         pass
+
     def test_highlight_provider(self):
         ''' Test highlight provider '''
         pass
+
     def test_reference_provider(self):
         ''' Test reference provider '''
         pass
+
     def test_rename_provider(self):
         ''' Test rename provider '''
         pass
+
     def transport_kind_opened(self):
         ''' Ensure the transport mechanism is properly opened '''
-        pass
+        conn = socket.create_connection((server.HOST, server.PORT))
+        print(conn)
+
     def transport_kind_closed(self):
         ''' Ensure the transport mechanism is properly closed '''
         pass
