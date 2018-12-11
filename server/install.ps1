8b7f01bfb0a59a110e89200f917375b5dca76497
Infosec-Intern <thomas@infosec-intern.com>
2018-12-10 21:28:03 -0700

    removed install.ps1

--- server/install.ps1
+++
@@ -1,8 +0,0 @@
-$Location = Split-Path -parent $PSCommandPath
-$PythonEnv = "$Location\env"
-$Pip = "$PythonEnv\Scripts\pip.exe"
-$Requirements = "yara-python"
-
-# Set-Variable INCLUDE="C:\Program Files (x86)\Windows Kits\10\Include"
-py -3 -m venv $PythonEnv
-& "$Pip install $Requirements"
