187af441f0195ae314773d2f7c93db9682a5223e
Infosec-Intern <thomas@infosec-intern.com>
2018-12-10 22:38:22 -0700

    keeping reader, writer local to handler

--- server/languageServer.py
+++ server/languageServer.py
@@ -16,8 +16,6 @@ class YaraLanguageServer(object):
         ''' Handle the details of the VSCode language server protocol '''
         self._logger = logging.getLogger("yara.server")
         self._separator=b"\r\n"
-        self.input = None
-        self.output = None
 
     async def handle_client(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         '''React and respond to client messages
@@ -26,15 +24,13 @@ class YaraLanguageServer(object):
         :writer: asyncio.StreamWriter. The connected client will read from this stream
         '''
         self._logger.info("Client connected")
-        self.input = reader
-        self.output = writer
         while True:
-            message = await self.read_request()
+            message = await self.read_request(reader)
             if message["method"] == "initialize":
                 announcement = await self.initialize()
-                await self.send_response(announcement)
+                await self.send_response(announcement, writer)
             elif message["method"] == "shutdown":
-                await self.remove_client()
+                await self.remove_client(writer)
                 self._logger.info("Disconnected client")
                 break
 
@@ -82,29 +78,29 @@ class YaraLanguageServer(object):
         ''' Respond to the textDocument/rename request '''
         self._logger.warning("provide_rename() is not yet implemented")
 
-    async def read_request(self) -> dict:
+    async def read_request(self, reader: asyncio.StreamReader) -> dict:
         ''' Read data from the client '''
-        data = await self.input.readuntil(separator=self._separator)
+        data = await reader.readuntil(separator=self._separator)
         key, value = tuple(data.decode().strip().split(" "))
         header = {key: value}
         self._logger.debug("%s %s", key, header[key])
         # read the extra line after the initial header
-        await self.input.readuntil(separator=self._separator)
+        await reader.readuntil(separator=self._separator)
         if key == "Content-Length:":
-            data = await self.input.readexactly(int(value))
+            data = await reader.readexactly(int(value))
         else:
-            data = await self.input.readuntil(separator=self._separator)
+            data = await reader.readuntil(separator=self._separator)
         self._logger.debug("in <= %r", data)
         return json.loads(data.decode())
 
-    async def remove_client(self):
-        ''' Close the output stream and remove the client connection '''
-        self.output.close()
-        await self.output.wait_closed()
+    async def remove_client(self, writer: asyncio.StreamWriter):
+        ''' Close the cient input & output streams '''
+        writer.close()
+        await writer.wait_closed()
 
-    async def send_response(self, response: dict):
+    async def send_response(self, response: dict, writer: asyncio.StreamWriter):
         ''' Write back to the client '''
         message = json.dumps(response).encode("utf-8")
         self._logger.debug("out => %s", message)
-        self.output.write(message)
-        await self.output.drain()
+        writer.write(message)
+        await writer.drain()
