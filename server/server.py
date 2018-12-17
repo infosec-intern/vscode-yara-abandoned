8e310e0b4ea0e23147f5da5b29d4e94adc8c5bf6
Infosec-Intern <thomas@infosec-intern.com>
2018-12-16 20:16:07 -0700

    reworked initialize and added JSONRPC metadata

--- server/server.py
+++ server/server.py
@@ -20,6 +20,10 @@ class YaraLanguageServer(object):
         self._encoding = "utf-8"
         self._eol=b"\r\n"
         self._logger = logging.getLogger("yara")
+        self.current_id = 0
+        self.reader = None
+        self.workspace = None
+        self.writer = None
 
     async def handle_client(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         '''React and respond to client messages
@@ -27,72 +31,60 @@ class YaraLanguageServer(object):
         :reader: asyncio StreamReader. The connected client will write to this stream
         :writer: asyncio.StreamWriter. The connected client will read from this stream
         '''
-        current_id = 0
         has_shutdown = False
         has_started = False
-        workspace = []
+        self.reader = reader
+        self.writer = writer
         self._logger.info("Client connected")
         while True:
-            message = await self.read_request(reader)
-            current_id = message["id"]
-            if current_id == 0:
+            message = await self.read_request()
+            self.current_id = message["id"]
+            if self.current_id == 0:
                 if message["method"] == "initialize":
-                    for folder in message["params"]["workspaceFolders"]:
-                        workspace.append(urlsplit(unquote(folder["uri"], encoding=self._encoding)).path)
-                    announcement = await self.initialize()
-                    await self.send_response(announcement, writer)
+                    self.workspace = urlsplit(unquote(message["params"]["rootUri"], encoding=self._encoding)).path
+                    self._logger.debug("Client workspace folder: %s", self.workspace)
+                    client_options = message.get("params", {}).get("capabilities", {}).get("textDocument", {})
+                    announcement = self.initialize(client_options)
+                    await self.send_response(announcement)
                     has_started = True
-                else: # client is trying to send data before server is initialized
-                    error = {
-                        "code": lsp.SERVER_NOT_INITIALIZED,
-                        "message": "Server has not initialized"
-                    }
-                    await self.send_response(error, writer)
+                else:
+                    # client is trying to send data before server is initialized
+                    await self.send_error(code=lsp.SERVER_NOT_INITIALIZED, msg="Server has not initialized")
+            elif has_started and message["method"] == "initialized":
+                self._logger.debug("Client has been successfully initialized")
             elif has_started and message["method"] == "shutdown":
+                self._logger.debug("Client requested shutdown")
                 has_shutdown = True
-                await self.send_response({}, writer)
+                await self.send_response({})
             elif has_started and message["method"] == "exit":
+                self._logger.debug("Client requested exit")
                 proper_shutdown = 0 if has_shutdown else 1
-                proper_exit = {
-                    "success": proper_shutdown
-                }
-                await self.send_response(proper_exit, writer)
-                await self.remove_client(writer)
+                await self.send_response({"success": proper_shutdown})
+                await self.remove_client()
 
-    async def initialize(self) -> dict:
+    def initialize(self, client_options: dict) -> dict:
         ''' Announce language support methods '''
-        return {
-            "capabilities": {
-                "textDocumentSync": lsp.TRANSPORTKIND_FULL
+        server_options = {}
+        if client_options.get("synchronization", {}).get("dynamicRegistration", False):
+            # Documents are synced by always sending the full content of the document
+            server_options["textDocumentSync"] = lsp.TRANSPORTKIND_FULL
+        if client_options.get("completion", {}).get("dynamicRegistration", False):
+            server_options["completionProvider"] = {
+                # The server does not provide support to resolve additional information for a completion item
+                "resolveProvider": False,
+                "triggerCharacters": ["."]
             }
-        #     "capabilities": {
-        #         "completionProvider" : {
-        #             # The server does not provide support to resolve additional information for a completion item
-        #             "resolveProvider": False,
-        #             "triggerCharacters": ["."]
-        #         },
-        #         "definitionProvider": True,
-        #         "documentFormattingProvider": True,
-        #         "documentHighlightProvider": True,
-        #         "referencesProvider": True,
-        #         "renameProvider": True,
-        #         "textDocumentSync": {
-        #             # Documents are synced by always sending the full content of the document
-        #             "change": 1,
-        #             "openClose": False,
-        #             # Save notifications are sent to the server
-        #             "save": True,
-        #             "willSave": False,
-        #             "willSaveWaitUntil": False
-        #         },
-        #         "workspace": {
-        #             "workspaceFolders": {
-        #                 "changeNotifications": False,
-        #                 "supported": False
-        #             }
-        #         }
-        #     }
-        }
+        if client_options.get("definition", {}).get("dynamicRegistration", False):
+            server_options["definitionProvider"] = True
+        if client_options.get("references", {}).get("dynamicRegistration", False):
+            server_options["referencesProvider"] = True
+        if client_options.get("documentHighlight", {}).get("dynamicRegistration", False):
+            server_options["documentHighlightProvider"] = True
+        if client_options.get("formatting", {}).get("dynamicRegistration", False):
+            server_options["documentFormattingProvider"] = True
+        if client_options.get("rename", {}).get("dynamicRegistration", False):
+            server_options["renameProvider"] = True
+        return {"capabilities": server_options}
 
     async def provide_code_completion(self) -> dict:
         ''' Respond to the completionItem/resolve request '''
@@ -106,7 +98,7 @@ class YaraLanguageServer(object):
         ''' Respond to the textDocument/publishDiagnostics request
         The message carries an array of diagnostic items for a resource URI.
         '''
-        self._logger.warning("provide_diagnostic() is not yet implemented")
+        self._logger.warning("provide_diagnostic() is not et implemented")
         if HAS_YARA:
             logging.debug("yara-python is installed")
         elif not HAS_YARA:
@@ -124,32 +116,59 @@ class YaraLanguageServer(object):
         ''' Respond to the textDocument/rename request '''
         self._logger.warning("provide_rename() is not yet implemented")
 
-    async def read_request(self, reader: asyncio.StreamReader) -> dict:
+    async def read_request(self) -> dict:
         ''' Read data from the client '''
-        data = await reader.readline()
+        data = await self.reader.readline()
+        self._logger.debug("in <= %r", data)
         key, value = tuple(data.decode(self._encoding).strip().split(" "))
         header = {key: value}
         self._logger.debug("%s %s", key, header[key])
         # read the extra separator after the initial header
-        await reader.readuntil(separator=self._eol)
+        await self.reader.readuntil(separator=self._eol)
         if key == "Content-Length:":
-            data = await reader.readexactly(int(value))
+            data = await self.reader.readexactly(int(value))
         else:
-            data = await reader.readline()
+            data = await self.reader.readline()
         self._logger.debug("in <= %r", data)
         return json.loads(data.decode(self._encoding))
 
-    async def remove_client(self, writer: asyncio.StreamWriter):
+    async def remove_client(self):
         ''' Close the cient input & output streams '''
-        writer.close()
-        await writer.wait_closed()
+        self.writer.close()
+        await self.writer.wait_closed()
         self._logger.info("Disconnected client")
 
-    async def send_response(self, response: dict, writer: asyncio.StreamWriter):
-        ''' Write back to the client '''
+    async def send_error(self, code: int, msg: str):
+        ''' Write back a JSON-RPC error message to the client '''
         message = json.dumps({
-            "result": response
+            "jsonrpc": "2.0",
+            "id": self.current_id,
+            "error": {
+                "code": code,
+                "message": msg
+            }
+        }).encode(self._encoding)
+        self._logger.debug("error => %s", message)
+        self.writer.write(message)
+        await self.writer.drain()
+
+    async def send_response(self, response: dict):
+        ''' Write back a JSON-RPC response to the client '''
+        message = json.dumps({
+            "jsonrpc": "2.0",
+            "id": self.current_id,
+            "result": response,
         }).encode(self._encoding)
         self._logger.debug("out => %s", message)
-        writer.write(message)
-        await writer.drain()
+        self.writer.write(message)
+        await self.writer.drain()
+
+    def show_message(self, msg_type: int, msg_text: str) -> dict:
+        ''' Ask the client to display a particular message in the user interface '''
+        return {
+            "method": "window/showMessageRequest",
+            "params": {
+                "type": msg_type,
+                "message": msg_text
+            }
+        }
