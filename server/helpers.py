3b445f7f77555b52e34a10f0ee6defa4a7ad8968
infosec-intern <thomas@infosec-intern.com>
2018-12-28 14:57:21 -0700

    filled out resolve_symbol()

--- server/helpers.py
+++ server/helpers.py
@@ -76,5 +76,22 @@ def resolve_symbol(document: str, pos: lsp.Position) -> str:
                To determine line numbers, text is split at newlines, and carriage returns are ignored
     :pos: Symbol position to base range off of
     '''
-    symbol = ""
-    return symbol
+    symbol_line = document.split("\n")[pos.line]
+    line_end = len(symbol_line)
+    # self._logger.debug("symbol line: %s", symbol_line)
+    # find the left-bound of the symbol by looking backwards until a whitespace
+    index = pos.char
+    while True:
+        if not symbol_line[index].strip():
+            lb = index + 1
+            break
+        index -= 1
+    # find the right-bound of the symbol by looking forwards until a whitespace
+    index = pos.char
+    while True:
+        if index == line_end or not symbol_line[index].strip():
+            rb = index
+            break
+        index += 1
+    # self._logger.debug("symbol: %s", symbol_line[lb:rb])
+    return symbol_line[lb:rb]
