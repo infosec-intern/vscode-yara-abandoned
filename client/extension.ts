6fb63d71e276f11699e717dafea3c3a2691c7959
infosec-intern <thomas@infosec-intern.com>
2019-02-18 21:46:03 -0700

    spawning server as child of client

--- client/extension.ts
+++ client/extension.ts
@@ -1,15 +1,29 @@
 "use strict";
 
+import {ChildProcess, spawn} from "child_process";
 import {Socket} from "net";
 import * as path from "path";
+import {platform} from "process";
 import {ExtensionContext, OutputChannel, window} from "vscode";
 import * as lcp from "vscode-languageclient";
 
 
 export function activate(context: ExtensionContext) {
-    let serverPath: string = context.asAbsolutePath(path.join("server", "runner.py"));
-    let pythonPath: string = context.asAbsolutePath(path.join("server", "env", "Scripts", "python"));
     let outputChannel: OutputChannel = window.createOutputChannel("YARA");
+    let serverPath: string = context.asAbsolutePath(path.join("server", "runner.py"));
+    let pythonPath: string = context.asAbsolutePath(path.join("server", "env", "bin", "python"));
+    if (platform === "win32") {
+        pythonPath = context.asAbsolutePath(path.join("server", "env", "Scripts", "python"));
+    }
+    // launch the language server
+    const options = {
+        cwd: context.asAbsolutePath(path.join("server")),
+        detached: false,
+        shell: false,
+        windowsHide: false
+    }
+    let langserver: ChildProcess = spawn(pythonPath, [serverPath], options);
+    // launch the client
     const serverOptions: lcp.ServerOptions = function() {
 		return new Promise((resolve, reject) => {
             var client = new Socket();
@@ -21,7 +35,30 @@ export function activate(context: ExtensionContext) {
 				});
 			});
 		});
-	}
+    }
+    langserver.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    langserver.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    langserver.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+    // let debugOptions = { execArgv: ["--nolazy"] };
+    // const serverOptions: lcp.ServerOptions = {
+    //     run: {
+    //         module: `${pythonPath} ${serverPath}`,
+    //         transport: lcp.TransportKind.ipc
+    //     },
+    //     debug: {
+    //         module: `${pythonPath} ${serverPath}`,
+    //         transport: lcp.TransportKind.ipc,
+    //         options: debugOptions
+    //     }
+    // }
     const clientOptions: lcp.LanguageClientOptions = {
         documentSelector: [{ scheme: "file", language: "yara" }],
         diagnosticCollectionName: "yara",
