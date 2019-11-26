c4ca69a2f3dd19df0009cea3abb5dec96a217fb4
Thomas Gardner <thomas@infosec-intern.com>
2019-11-26 09:59:48 -0700

    added test_rules fixture and unix/windows markers

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,6 +1,7 @@
 """ Reusable test fixtures """
 import asyncio
 import logging
+from pathlib import Path
 
 import pytest
 
@@ -12,17 +13,21 @@ def pytest_configure(config):
     config.addinivalue_line("markers", "protocol: Run language server protocol unittests")
     config.addinivalue_line("markers", "server: Run YARA-specific protocol unittests")
     config.addinivalue_line("markers", "transport: Run network transport unittests")
+    config.addinivalue_line("markers", "unix: Tests specific to Unix-based OSes")
+    config.addinivalue_line("markers", "windows: Tests specific to the Windows OS")
 
 def noop(x):
-    """ No-operation function
-        Used as a default for network servers
-        when no callback is provided
-    """
+    """ No-operation function """
     pass
 
 @pytest.fixture(scope="function")
-async def local_server(event_loop, unused_tcp_port, cb=noop):
-    """ Set up a local asyncio network server """
+async def local_server(unused_tcp_port, cb=noop):
+    """Set up a local asyncio network server
+
+    :param unused_tcp_port: Random TCP port to bind server to. Provided by pytest-asyncio
+    :param cb: Callback function for server to run. Defaults to a no-op function called noop()
+    :return: Address and port that server is bound to
+    """
     addr = "localhost"
     port = unused_tcp_port
     server = await asyncio.start_server(
@@ -37,3 +42,9 @@ async def local_server(event_loop, unused_tcp_port, cb=noop):
     server.close()
     await server.wait_closed()
     logging.debug("Server closed")
+
+@pytest.fixture(scope="function")
+def test_rules():
+    """ Resolve full path to the test YARA rules """
+    rules_path = Path(__file__).parent.joinpath("..", "..", "test", "rules")
+    return rules_path.resolve()
