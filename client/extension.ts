31373755fc2b85ec58e7baf406d35fc51f33cebc
Thomas Gardner <thomas@infosec-intern.com>
2019-12-06 16:42:16 -0700

    updated client and server to gen rando tcp port and spawn the server the "newer" way

--- client/extension.ts
+++ client/extension.ts
@@ -6,15 +6,19 @@ import * as path from "path";
 import {platform} from "process";
 import {ExtensionContext, OutputChannel, window} from "vscode";
 import * as lcp from "vscode-languageclient";
+import * as getPort from "get-port";
 
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
     let outputChannel: OutputChannel = window.createOutputChannel("YARA");
-    let serverPath: string = context.asAbsolutePath(path.join("server", "runner.py"));
+    let serverPath: string = context.asAbsolutePath(path.join("server", "vscode_yara.py"));
     let pythonPath: string = context.asAbsolutePath(path.join("server", "env", "bin", "python"));
     if (platform === "win32") {
         pythonPath = context.asAbsolutePath(path.join("server", "env", "Scripts", "python"));
     }
+    let lhost: string = "127.0.0.1";
+    // grab a random open TCP port to listen to
+    let tcpPort: number = await getPort();
     // launch the language server
     const options = {
         cwd: context.asAbsolutePath(path.join("server")),
@@ -22,48 +26,20 @@ export function activate(context: ExtensionContext) {
         shell: false,
         windowsHide: false
     }
-    let langserver: ChildProcess = spawn(pythonPath, [serverPath], options);
-    // launch the client
-    const serverOptions: lcp.ServerOptions = function() {
-		return new Promise((resolve, reject) => {
-            var client = new Socket();
-			client.connect(8471, "127.0.0.1", function() {
-                console.log(`client connected to ${client.localAddress}:${client.localPort}`);
-				resolve({
-					reader: client,
-					writer: client
-				});
-			});
-		});
-    }
-    langserver.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-
-    langserver.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
-    });
-
-    langserver.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
-    // let debugOptions = { execArgv: ["--nolazy"] };
-    // const serverOptions: lcp.ServerOptions = {
-    //     run: {
-    //         module: `${pythonPath} ${serverPath}`,
-    //         transport: lcp.TransportKind.ipc
-    //     },
-    //     debug: {
-    //         module: `${pythonPath} ${serverPath}`,
-    //         transport: lcp.TransportKind.ipc,
-    //         options: debugOptions
-    //     }
-    // }
+    // env/bin/python script.py <lhost> <lport>
+    let langserver: lcp.Executable = {
+        command: pythonPath,
+        args: [serverPath, lhost, tcpPort.toString()],
+        options: options
+    };
+    const serverOptions: lcp.ServerOptions = {
+        run: langserver,
+        debug: langserver
+    };
     const clientOptions: lcp.LanguageClientOptions = {
         documentSelector: [{ scheme: "file", language: "yara" }],
         diagnosticCollectionName: "yara",
         outputChannel: outputChannel,
-        synchronize: { configurationSection: "yara" }
     };
     let client = new lcp.LanguageClient(
         "yara-languageclient",
--- server/vscode_yara.py
+++ server/vscode_yara.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import argparse
 import asyncio
 import json
 import logging
@@ -19,14 +19,21 @@ logger.addHandler(file_hdlr)
 logger.setLevel(logging.DEBUG)
 
 
+def _build_cli():
+    parser = argparse.ArgumentParser(description="Start the vscode-yara language server")
+    parser.add_argument("host", help="Interface to bind server to")
+    parser.add_argument("port", type=int, help="Port to bind server to")
+    return parser.parse_args()
+
 async def main():
     ''' Program entrypoint '''
+    args = _build_cli()
     yarals = YaraLanguageServer()
     logger.info("Starting YARA IO language server")
     socket_server = await asyncio.start_server(
         client_connected_cb=yarals.handle_client,
-        host="127.0.0.1",
-        port=8471,
+        host=args.host,
+        port=args.port,
         start_serving=False
     )
     servhost, servport = socket_server.sockets[0].getsockname()
@@ -37,5 +44,7 @@ async def main():
     except asyncio.CancelledError:
         logger.info("Server has successfully shutdown")
 
-
-asyncio.run(main(), debug=True)
+try:
+    asyncio.run(main(), debug=True)
+except KeyboardInterrupt:
+    logger.info("Ending per user request")
