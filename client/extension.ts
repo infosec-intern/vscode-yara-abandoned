6d47dcd7d7f7b8e8df0b334bdb84c28ebf7ba530
Thomas Gardner <thomas@infosec-intern.com>
2019-12-09 10:19:31 -0700

    changed serverOptions back to connection callback added install_server skeleton

--- client/extension.ts
+++ client/extension.ts
@@ -9,7 +9,24 @@ import * as lcp from "vscode-languageclient";
 import * as getPort from "get-port";
 
 
-function start_server(context: ExtensionContext, lhost: string, tcpPort: number): lcp.ServerOptions {
+function install_server(context: ExtensionContext): boolean {
+    /*
+        generate the python runtime & install necessary packages
+        returns true or false if installation was successful
+    */
+    // get the local directory where the env will be installed
+    let envPath: string = context.asAbsolutePath(path.join("server", "env"));
+    // check if python3.6+ and the "virtualenv" package are installed
+    // if not, notify the user and error out
+    // else execute the appropriate command to create a Python3 environment
+    return false;
+}
+
+function start_server(context: ExtensionContext, lhost: string, tcpPort: number): ChildProcess {
+    /*
+        start up the language server with the pre-installed python runtime
+        returns the language server's ChildProcess instance
+    */
     let serverPath: string = context.asAbsolutePath(path.join("server", "vscode_yara.py"));
     let pythonPath: string = context.asAbsolutePath(path.join("server", "env", "bin", "python"));
     if (platform === "win32") {
@@ -23,16 +40,8 @@ function start_server(context: ExtensionContext, lhost: string, tcpPort: number)
         windowsHide: false
     }
     // env/bin/python server.py <lhost> <lport>
-    let langserver: lcp.Executable = {
-        command: pythonPath,
-        args: [serverPath, lhost, tcpPort.toString()],
-        options: options
-    };
-    const serverOptions: lcp.ServerOptions = {
-        run: langserver,
-        debug: langserver
-    };
-    return serverOptions;
+    let langserver: ChildProcess = spawn(pythonPath, [serverPath, lhost, tcpPort.toString()], options);
+    return langserver;
 }
 
 export async function activate(context: ExtensionContext) {
@@ -40,7 +49,21 @@ export async function activate(context: ExtensionContext) {
     let lhost: string = "127.0.0.1";
     // grab a random open TCP port to listen to
     let tcpPort: number = await getPort();
-    let serverOptions: lcp.ServerOptions = start_server(context, lhost, tcpPort);
+    let langserver: ChildProcess = start_server(context, lhost, tcpPort);
+    // when the client starts it should open a socket to the server
+    const serverOptions: lcp.ServerOptions = function() {
+        return new Promise((resolve, reject) => {
+            console.log(`Attempting connection to tcp://${lhost}:${tcpPort}`);
+            var client = new Socket();
+            client.connect(tcpPort, lhost, function() {
+                resolve({
+                    reader: client,
+                    writer: client
+                });
+            });
+        });
+    }
+    // register the client for all the YARA things
     const clientOptions: lcp.LanguageClientOptions = {
         documentSelector: [{ scheme: "file", language: "yara" }],
         diagnosticCollectionName: "yara",
@@ -49,15 +72,13 @@ export async function activate(context: ExtensionContext) {
             configurationSection: "yara",
         }
     };
-    // get the client to start the server
     let client = new lcp.LanguageClient(
         "yara-languageclient",
         "YARA",
         serverOptions,
         clientOptions
     );
-    client.info(`Attempting connection to tcp://${lhost}:${tcpPort}`);
-    client.info("Connected to YARA Language Server");
+    client.info("Started YARA extension");
     context.subscriptions.push(client.start());
 }
 
